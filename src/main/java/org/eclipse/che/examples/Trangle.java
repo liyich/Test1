d07da8644a11a4eb6379f7520a2eb0b5224ff3e0
le'e <coding16@gmail.com>
2016-07-24 14:57:53 +0000

    1

---
+++ src/main/java/org/eclipse/che/examples/Trangle.java
@@ -0,0 +1,17 @@
+package org.eclipse.che.examples;
+
+public class Trangle {
+    private int sides;
+    private String name;
+    public Trangle(){
+        this.sides = 3;
+        this.name = "helasad ";
+    }
+    public Trangle(int sides, String name){
+        this.sides = sides;
+        this. name = name;
+    }
+    public void output(){
+        System.out.println(sides+""+name);
+    }
+}
