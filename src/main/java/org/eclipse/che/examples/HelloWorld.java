bb6d0ed79d5eb2828c6fee097039ce89e81fa342
le'e <coding16@gmail.com>
2016-07-24 14:49:26 +0000

    test1

---
+++ src/main/java/org/eclipse/che/examples/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.eclipse.che.examples;
+
+public class HelloWorld {
+    public static void main(String[] argvs) {
+        String a = "Che";
+        System.out.println("Hello World " + a + "!");
+        System.out.println();
+        System.out.println();
+    }
+}
